4cb0671d35fa50272da712049628a011242fd62f
Krzysztof Żebrowski <krzysztof.zebrowski@mail.com>
2019-09-22 19:47:58 +0200

    Adding Bootstrap try n.o. 1

--- ReservationSystem/ClientApp/src/components/Layout.js
+++ ReservationSystem/ClientApp/src/components/Layout.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import { Col, Grid, Row } from 'react-bootstrap';
-import { NavMenu } from './NavMenu';
+import  { Container, Row, Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem}  from 'reactstrap';
 
 export class Layout extends Component {
   displayName = Layout.name
 
   render() {
     return (
-      <Grid fluid>
+      <Container>
         <Row>
-          <Col sm={3}>
-            <NavMenu />
-          </Col>
-          <Col sm={9}>
-            {this.props.children}
-          </Col>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={true} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
         </Row>
-      </Grid>
+        {this.props.children}
+      </Container>
     );
   }
 }
