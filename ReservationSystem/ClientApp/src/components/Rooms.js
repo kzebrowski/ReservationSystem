0cb5c3e38f75f415e8a40ae07329d8193d781209
Krzysztof Żebrowski <krzysztof.zebrowski@mail.com>
2019-11-17 17:24:24 +0100

    Fixed inifinite loop bug

--- ReservationSystem/ClientApp/src/components/Rooms.js
+++ ReservationSystem/ClientApp/src/components/Rooms.js
@@ -8,23 +8,29 @@ export default class Rooms extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { rooms: [], loading: true }
+    this.state = { rooms: [], loading: true, oldParams: {data: "ddd"}}
     this.componentDidUpdate();
   }
 
   componentDidUpdate() {
     let params = this.props.match.params;
-
-    if (params.startDate && params.endDate && params.numberOfGuests){
-      this.fetchData(`api/SampleData/Search?startDate=${params.startDate}&endDate=${params.endDate}&numberOfGuests=${params.numberOfGuests}`);
+    
+    if (params == this.state.oldParams){
+      return;
     }
-    else {
-      this.fetchData('api/SampleData/Rooms');
+    else{
+      this.setState({oldParams: params});
+      if (params.startDate && params.endDate && params.numberOfGuests){
+        this.fetchData(`api/SampleData/Search?startDate=${params.startDate}&endDate=${params.endDate}&numberOfGuests=${params.numberOfGuests}`);
+      }
+      else {
+        this.fetchData('api/SampleData/Rooms');
+      }
     }
   }
 
   fetchData(url) {
-    this.state = { loading: true }
+    this.setState({ loading: true });
 
     fetch(url)
       .then(response => response.json())
