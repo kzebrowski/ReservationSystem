e3a2193f05c19e393bcb741c57d1d21653dc1f07
Krzysztof Żebrowski <krzysztof.zebrowski@mail.com>
2020-01-12 15:55:19 +0100

    Added homepage carousel

--- ReservationSystem/ClientApp/src/components/Home.js
+++ ReservationSystem/ClientApp/src/components/Home.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
 
 export class Home extends Component {
   displayName = Home.name
 
+  items = [
+    {
+      src: 'https://res.cloudinary.com/dwcl9sgyd/image/upload/v1578491376/ReservationSystem/design_ktm416.jpg'
+    },
+    {
+      src: 'https://res.cloudinary.com/dwcl9sgyd/image/upload/v1578840614/ReservationSystem/BIH_164_aspect16x9_xb4p4k.jpg'
+    },
+    {
+      src: 'https://res.cloudinary.com/dwcl9sgyd/image/upload/v1578491353/ReservationSystem/1-hotel-central-park-1_tgwa6s.jpg'
+    }
+  ];
+
   render() {
     return (
       <div>
-        <h1>Tu będzie strona główna</h1>
+        <UncontrolledCarousel items={this.items} />
       </div>
     );
   }
