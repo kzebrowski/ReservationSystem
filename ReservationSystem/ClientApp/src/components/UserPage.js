ca8db0e2ff6bc03131689bd8dad87b592cce7083
Krzysztof Żebrowski <krzysztof.zebrowski@mail.com>
2020-01-23 09:51:49 +0100

    Added confirmation modal for reservation canceling

--- ReservationSystem/ClientApp/src/components/UserPage.js
+++ ReservationSystem/ClientApp/src/components/UserPage.js
@@ -6,24 +6,27 @@ import PulseLoader from 'react-spinners/PulseLoader';
 import './styles/UserPage.css';
 import Axios from 'axios';
 import InformationModal from './InformationModal';
+import ConfirmationModal from './ConfirmationModal';
 
 export default class UserPage extends Component {
   displayName = UserPage.name;
 
   constructor(props) {
     super(props);
-    this.state = {reservations: {}, reservationsLoading: true, modalmessage: ''};
+    this.state = {reservations: {}, reservationsLoading: true, modalmessage: '', confirmationModalData: {isOpen: false, id: ''}};
 
     this.fetchReservations = this.fetchReservations.bind(this);
 
-    this.handleCancelReservation = this.handleCancelReservation.bind(this);
     this.showMessage = this.showMessage.bind(this);
     this.clearMessage = this.clearMessage.bind(this);
+    this.openConfirmationModal = this.openConfirmationModal.bind(this);
+    this.hideConfirmationModal = this.hideConfirmationModal.bind(this);
+    this.cancelReservation = this.cancelReservation.bind(this);
 
     this.fetchReservations();
   }
-  
-  renderCancelButton = x => x.status === 'Anulowane' ? '' : <ActionIcon icon={faTimes} itemId={x.id} handleClick={this.handleCancelReservation}/>;
+
+  renderCancelButton = x => x.status === 'Anulowane' ? '' : <ActionIcon icon={faTimes} itemId={x.id} handleClick={this.openConfirmationModal}/>;
 
   fetchReservations() {
     this.setState({ reservationsLoading: true });
@@ -32,7 +35,23 @@ export default class UserPage extends Component {
       .then(response => this.setState({ reservations: response.data, reservationsLoading: false}));
   }
 
-  handleCancelReservation(id) {
+  clearMessage() {
+    this.setState({modalmessage: ''});
+  }
+
+  showMessage(message) {
+    this.setState({modalmessage: message});
+  }
+
+  openConfirmationModal(id) {
+    this.setState({confirmationModalData: {id: id, isOpen: true} });
+  }
+
+  hideConfirmationModal() {
+    this.setState({confirmationModalData: {isOpen: false} });
+  }
+
+  cancelReservation(id) {
     this.setState({reservationsLoading: true});
 
     Axios.post("/api/reservations/cancel", 
@@ -42,15 +61,7 @@ export default class UserPage extends Component {
         this.showMessage("Rezerwacja została anulowana.");
         this.fetchReservations()})
       .catch(() => this.showMessage("Wystąpił błąd. Spróbuj ponownie."))
-      .finally(() => this.setState({reservationsLoading: false}));
-  }
-
-  clearMessage() {
-    this.setState({modalmessage: ''});
-  }
-
-  showMessage(message) {
-    this.setState({modalmessage: message});
+      .finally(() => this.setState({reservationsLoading: false, confirmationModalData: {isOpen: false}}));
   }
 
   render() {
@@ -103,6 +114,11 @@ export default class UserPage extends Component {
           </tbody>
         </Table>
         <InformationModal isOpen={this.state.modalmessage !== ''} message={this.state.modalmessage} handleOkay={this.clearMessage} />
+        <ConfirmationModal isOpen={this.state.confirmationModalData.isOpen}
+          message="Czy na pewno chcesz anulować rezerwację?"
+          data={this.state.confirmationModalData.id}
+          handleYes={this.cancelReservation}
+          handleNo={this.hideConfirmationModal} />
     </div>);
   }
 }
