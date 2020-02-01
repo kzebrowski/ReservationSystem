fb97d10ef578e4d0f757e388a59fb50810ffec29
Krzysztof Żebrowski <krzysztof.zebrowski@mail.com>
2020-02-01 17:31:49 +0100

    Added fancy sections to front page

--- ReservationSystem/ClientApp/src/components/Home.js
+++ ReservationSystem/ClientApp/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { UncontrolledCarousel } from 'reactstrap';
+import RoomSearch from './RoomSearch';
 
 export class Home extends Component {
   displayName = Home.name
@@ -20,6 +21,28 @@ export class Home extends Component {
     return (
       <div>
         <UncontrolledCarousel items={this.items} />
+        <div className="front-page-section">
+          <div className="vertically-centered">
+            <h2 className="d-flex justify-content-center">Poznaj nasz nowoczesny komplets wypoczynkowy!</h2>
+            <h6 className="pt-2 d-flex justify-content-center">Nunc elementum nulla ut augue rutrum pharetra. Morbi maximus pharetra justo, sed tempus urna varius sit amet. 
+              Curabitur sagittis, nulla eleifend sollicitudin laoreet, lorem odio luctus nunc, ut ullamcorper leo tellus eu orci. Ut egestas tempus viverra. 
+              Vestibulum non finibus lacus. In nec tristique justo. Proin varius luctus dui, id dignissim purus hendrerit a. Duis ut nisl vel mi lacinia porttitor. 
+              Mauris iaculis vel mi a ultricies.</h6>
+          </div>
+        </div>
+        <div className="front-page-section">
+          <div className="vertically-centered">
+            <h2 className="d-flex justify-content-center">Znajdź pokój dla siebie.</h2>
+            <h6 className="pt-2 d-flex justify-content-center">Wpisz daty swojego pobytu i sprawdź dostępność naszych pokoi.</h6>
+            <RoomSearch />
+          </div>
+        </div>
+        <div className="front-page-section">
+          <div className="vertically-centered">
+            <h2 className="d-flex justify-content-center">U nas znajdziesz wszystko to, czego potrzebujesz.</h2>
+            <h6 className="pt-2 d-flex justify-content-center">Tutaj jakieś ikonki wstawić.</h6>
+          </div>
+        </div>
       </div>
     );
   }
